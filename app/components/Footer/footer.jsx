841805bdd96f183930fc5c6ab825242bbcc1c806
Mona <m.yousefinejad78@gmail.com>
2025-03-07 15:02:36 +0330

    icons were added to the footer

---
+++ app/components/Footer/footer.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react'
+import { IoLogoInstagram,IoLogoYoutube } from "react-icons/io5";
+import { LuTwitter } from "react-icons/lu";
+const Footer  = () => {
+  return (
+    <div className='bg-slate-700 flex gap-3 p-3 mt-10 justify-evenly'>
+        <div className='flex gap-3'>
+            <Link href='/'><IoLogoInstagram className='text-white text-2xl' /></Link>
+            <Link href='/'><LuTwitter className='text-white text-2xl'/></Link>
+            <Link href='/'><IoLogoYoutube className='text-white text-2xl' /></Link>
+        </div>
+        <h1 className='text-white'>تمامی حقوق محفوظ است</h1>
+    </div>
+  )
+}
+
+export default Footer 
