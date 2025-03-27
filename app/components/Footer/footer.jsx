3764cc30a583c7c2a389388056879d476449e73e
Mona <m.yousefinejad78@gmail.com>
2025-03-27 12:19:08 +0330

    footer was fixed and i removed it and the position modified

--- app/components/Footer/footer.jsx
+++ app/components/Footer/footer.jsx
@@ -4,7 +4,7 @@ import { IoLogoInstagram,IoLogoYoutube } from "react-icons/io5";
 import { LuTwitter } from "react-icons/lu";
 const Footer  = () => {
   return (
-    <div className=' z-50 bg-slate-700 flex gap-3 p-3 mt-10 bottom-0 justify-evenly fixed w-full'>
+    <div className='z-50 bg-slate-700 flex gap-3 p-3 mt-10 bottom-0 justify-evenly w-full'>
         <div className='flex gap-3'>
             <Link href='/'><IoLogoInstagram className='text-white text-2xl' /></Link>
             <Link href='/'><LuTwitter className='text-white text-2xl'/></Link>
