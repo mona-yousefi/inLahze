9029487d00a584e5d378bd3adb01632083070b6c
Mona <m.yousefinejad78@gmail.com>
2025-03-30 14:38:06 +0330

    adding alt property to the image

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex justify-center">
-        <Image src="/spiritual-retreat-with-man-meditating_23-2149325117.jpg" width={1200} height={400} layout="responsive" className="mt-0"/>
+        <Image src="/spiritual-retreat-with-man-meditating_23-2149325117.jpg" priority="true" alt="man sitting in peace" width={1200} height={400} layout="responsive" className="mt-0"/>
       </div>
       <div className="flex flex-col items-center bg-gray-100 mt-11 px-2">
         <p className="text-3xl">دسترسی سریع</p>
