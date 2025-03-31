fb1fc00f66c93f1e60b37dc50b4d9b95fda56ad6
Mona <m.yousefinejad78@gmail.com>
2025-03-31 21:20:09 +0330

    added persian description to mock data

--- app/MockData/mock.js
+++ app/MockData/mock.js
@@ -2,30 +2,30 @@
 export const mockProducts = [
     {
       id: 1,
-      title: 'Wireless Headphones',
-      image: 'https://via.placeholder.com/300x200?text=Wireless+Headphones',
-      price: 99.99,
+      title: 'شروع بیداری',
+      image: "/1723632939-xG9BcbgG.jpg",
+      price: 255000,
       description: 'High-quality wireless headphones with noise cancellation'
     },
     {
       id: 2,
-      title: 'Smart Watch',
-      image: 'https://via.placeholder.com/300x200?text=Smart+Watch',
-      price: 199.99,
+      title: 'عبور از ترس',
+      image: "/1723635109-5Ya7V8qd.jpg",
+      price: 200000,
       description: 'Latest smart watch with health monitoring'
     },
     {
       id: 3,
-      title: 'Bluetooth Speaker',
-      image: 'https://via.placeholder.com/300x200?text=Bluetooth+Speaker',
-      price: 59.99,
+      title: 'سه گانه شروع از نو',
+      image: "/1723634054-U1ke3TOf.jpg",
+      price: 150000,
       description: 'Portable speaker with 20hr battery life'
     },
     {
       id: 4,
-      title: 'Laptop Backpack',
-      image: 'https://via.placeholder.com/300x200?text=Laptop+Backpack',
-      price: 49.99,
+      title: 'تو از نو',
+      image: "/1723634786-hfYV7R28.jpg",
+      price: 250000,
       description: 'Durable backpack with laptop compartment'
     },
     {
@@ -33,13 +33,13 @@ export const mockProducts = [
       title: 'سه گانه شروع از نو',
       image: "/1723634054-U1ke3TOf.jpg",
       price: 150000,
-      description: 'Portable speaker with 20hr battery life'
+      description: 'این بسته شامل سه دوره  شروع بیداری، عبور از ترس و  تو از نو می باشد و با تخفیف ارائه می شود'
     },
     {
       id: 6,
       title: 'تو از نو',
       image: "/1723634786-hfYV7R28.jpg",
       price: 250000,
-      description: 'Durable backpack with laptop compartment'
+      description: 'رهایی از بند وابستگی، افزایش قدرت اراده و یافتن عظمت درون'
     }
   ];
