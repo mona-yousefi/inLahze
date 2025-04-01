5f634f5c189817dc219636ee150341fb6d49abd3
Mona <m.yousefinejad78@gmail.com>
2025-04-02 00:53:41 +0330

    deleting unwanted data from sign in and sign up

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,14 +1,3 @@
-// import SignInForm from '../components/SignInForm/signInForm';
-
-// const SignIn = () => {
-//   return (
-//     <div className='bg-custom_img bg-cover h-screen'>
-//           <SignInForm/>
-//     </div>
-//   )
-// }
-
-// export default SignIn
 import SignInForm from '../components/SignInForm/signInForm';
 
 const SignIn = () => {
--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,15 +1,3 @@
-// import React from 'react'
-// import SignUpForm from '../components/SignUpForm/signUpForm'
-
-// const SignUp = () => {
-//   return (
-//     <div className='bg-sign_up_image bg-cover'>
-//       <SignUpForm/>
-//     </div>
-//   )
-// }
-
-// export default SignUp
 import React from 'react'
 import SignUpForm from '../components/SignUpForm/signUpForm'
 
