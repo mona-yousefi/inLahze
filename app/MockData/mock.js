879858b09995525b0d491a0a3fd31cf1a556bd89
Mona <m.yousefinejad78@gmail.com>
2025-04-01 13:06:59 +0330

    mock data updated again

--- app/MockData/mock.js
+++ app/MockData/mock.js
@@ -5,28 +5,28 @@ export const mockProducts = [
       title: 'شروع بیداری',
       image: "/1723632939-xG9BcbgG.jpg",
       price: 255000,
-      description: 'High-quality wireless headphones with noise cancellation'
+      description: 'ولین دوره مدیتیشن،برای تو که تصمیم داری تمرین مدیتیشن را عمیق تر دنبال کنی'
     },
     {
       id: 2,
       title: 'عبور از ترس',
       image: "/1723635109-5Ya7V8qd.jpg",
       price: 200000,
-      description: 'Latest smart watch with health monitoring'
+      description: 'همه ما در زندگـــی دچــار ترس و نــگرانی و شــک شــده ایــم.احســاس ناتـــوانی و شکســت را بارها و بارها تجــربه کرده ایم.'
     },
     {
       id: 3,
       title: 'سه گانه شروع از نو',
       image: "/1723634054-U1ke3TOf.jpg",
       price: 150000,
-      description: 'Portable speaker with 20hr battery life'
+      description: 'این بسته شامل سه دوره  شروع بیداری، عبور از ترس و  تو از نو می باشد و با تخفیف ارائه می شود'
     },
     {
       id: 4,
       title: 'تو از نو',
       image: "/1723634786-hfYV7R28.jpg",
       price: 250000,
-      description: 'Durable backpack with laptop compartment'
+      description: 'رهایی از بند وابستگی، افزایش قدرت اراده و یافتن عظمت درون'
     },
     {
       id: 5,
